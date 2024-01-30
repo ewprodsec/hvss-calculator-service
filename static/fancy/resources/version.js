3c112d5100bd95805e0b42db6066aa53424465a4
Jacob Barkai <jacob_barkai@edwards.com>
2024-01-30 21:35:40 +0200

    Updated ML models and training data file. ML models build ID: d8437.

--- static/fancy/resources/version.js
+++ static/fancy/resources/version.js
@@ -1,2 +1,2 @@
-const ml_models_build_date = '2023-12-21-132907+0200';
-const ml_models_build_id = '55572';
+const ml_models_build_date = '2024-01-30-212929+0200';
+const ml_models_build_id = 'd8437';
